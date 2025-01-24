50fc032a24de48cedc99174ec85e6a829f4fffc5
Joseph Olayanju <horlarhyanjuhjoseph@gmail.com>
2025-01-24 21:44:17 +0100

    set up user interface

--- src/interface/user.interface.ts
+++ src/interface/user.interface.ts
@@ -1,5 +1,16 @@
-export interface User {
-  id: string;
-  name: string;
+import { Document } from 'mongoose';
+export interface IUser {
+  firstname: string;
+  lastname: string;
   email: string;
+  password: string;
+  access_level: number;
+  country: string;
+}
+
+export interface IUserDocument extends IUser, Document {}
+
+export interface IUserAuth extends IUser, Pick<IUserDocument, 'id'> {
+  access_token: string;
+  refresh_token: string;
 }
