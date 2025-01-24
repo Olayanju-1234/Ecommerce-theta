c3a2a34104093867c0c384a7ac39f9cf078ea046
Joseph Olayanju <horlarhyanjuhjoseph@gmail.com>
2025-01-24 23:46:59 +0100

    local sign up controller

---
+++ src/controllers/auth.ts
@@ -0,0 +1,50 @@
+import { errorResponse, successResponse } from '../utils';
+import { body, query } from 'express-validator';
+import { LocalSignUp, SignToken } from '../services/Auth';
+import { Request, Response } from 'express';
+
+export const validateRequests = (method: string) => {
+  switch (method) {
+    case 'signup': {
+      return [
+        body('email', 'Invalid email').isEmail(),
+        body('password', 'Password must be at least 6 characters').isLength({
+          min: 6,
+        }),
+        body('firstname', 'Firstname is required').exists(),
+        body('lastname', 'Lastname is required').exists(),
+        body('country', 'Country is required').exists(),
+      ];
+    }
+    default:
+      return [
+        query('email', 'Invalid email').isEmail(),
+        query('password', 'Password must be at least 6 characters').isLength({
+          min: 6,
+        }),
+      ];
+  }
+};
+
+export const localRegistration = async (req: Request, res: Response) => {
+  try {
+    const { email, password, firstname, lastname, country } = req.body;
+
+    const response = await LocalSignUp({
+      email,
+      password,
+      firstname,
+      lastname,
+      country,
+    });
+
+    const userToken = await SignToken({
+      _id: response.user._id as any,
+      access_level: response.user.access_level,
+    });
+
+    return successResponse(res, 201, { ...response, user: userToken });
+  } catch (error: any) {
+    return errorResponse(res, 500, error.message);
+  }
+};
