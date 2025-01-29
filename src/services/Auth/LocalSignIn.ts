b48e654ae480ee95fa2b3d62ca3adabf08867f11
Joseph Olayanju <horlarhyanjuhjoseph@gmail.com>
2025-01-29 12:22:12 +0100

    local login service

---
+++ src/services/Auth/LocalSignIn.ts
@@ -0,0 +1,18 @@
+import { SignToken } from './SignToken';
+import User from '../../models/user.model';
+import { compare } from 'bcryptjs';
+
+export const LocalSignIn = async (email: string, password: string) => {
+  let user = await User.findOne({ email });
+
+  if (!user) throw new Error('User not found');
+
+  const isMatch = await compare(password, user.password);
+
+  if (!isMatch) throw new Error('Invalid password');
+
+  return SignToken({
+    _id: user._id as string,
+    access_level: user.access_level,
+  });
+};
