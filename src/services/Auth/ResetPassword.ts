605c1656ee684951f2e0467870306e10d697377f
Joseph Olayanju <horlarhyanjuhjoseph@gmail.com>
2025-02-01 14:16:08 +0100

    feat: reset password

--- src/services/Auth/ResetPassword.ts
+++ src/services/Auth/ResetPassword.ts
@@ -1,18 +1,41 @@
 import UserModel from '../../models/user.model';
 import { hash } from 'bcryptjs';
+import { SendEmailFromTemplate } from '../Email';
 
 export const ResetPassword = async (
   reset_password_token: string,
   password: string,
 ) => {
   const user = await UserModel.findOne({ reset_password_token });
-  if (!user || !user.reset_password_expires) {
-    throw new Error('Invalid or expired reset password token');
+
+  if (!user) {
+    throw new Error('Invalid reset password token');
   }
+
+  if (user.reset_password_expires && user.reset_password_expires < Date.now()) {
+    throw new Error('Reset password token has expired');
+  }
+
   const hashedPassword = await hash(password, 10);
-  user.password = hashedPassword;
-  user.reset_password_token = undefined;
-  user.reset_password_expires = undefined;
-  await user.save();
-  return user;
+
+  await UserModel.updateOne(
+    { reset_password_token },
+    {
+      password: hashedPassword,
+      reset_password_token: null,
+      reset_password_expires: null,
+    },
+  );
+
+  await SendEmailFromTemplate({
+    template: 'ResetPassword',
+    from: '[email protected]',
+    to: user.email,
+    data: {
+      firstname: user.firstname,
+      email: user.email,
+    },
+  });
+
+  return true;
 };
