8d04e3ca5b6715549a6b238109e24f1602d80df1
Joseph Olayanju <horlarhyanjuhjoseph@gmail.com>
2025-02-01 14:16:18 +0100

    feat: forgot password

--- src/services/Auth/ForgotPassword.ts
+++ src/services/Auth/ForgotPassword.ts
@@ -1,16 +1,31 @@
 import { nanoid } from 'nanoid';
 import UserModel from '../../models/user.model';
+import { SendEmailFromTemplate } from '../Email';
 
 export const ForgotPassword = async (email: string) => {
   const user = await UserModel.findOne({ email });
+
   if (!user) {
     throw new Error('User not found');
   }
 
-  const reset_password_token = nanoid(10);
+  const reset_password_token = nanoid(6).toUpperCase();
 
-  user.reset_password_token = reset_password_token;
-  user.reset_password_expires = new Date(Date.now() + 3600000);
-  await user.save();
-  return user;
+  await UserModel.updateOne(
+    { email },
+    { reset_password_token, reset_password_expires: Date.now() + 3600000 },
+  );
+
+  await SendEmailFromTemplate({
+    template: 'ForgotPassword',
+    from: '[email protected]',
+    to: email,
+    data: {
+      firstname: user.firstname,
+      email: user.email,
+      token: reset_password_token,
+    },
+  });
+
+  return true;
 };
