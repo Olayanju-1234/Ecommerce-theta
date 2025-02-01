3093107631b259acc44d03d10d47b210b55caf15
Joseph Olayanju <horlarhyanjuhjoseph@gmail.com>
2025-02-01 14:16:30 +0100

    feat: email verification

---
+++ src/services/Auth/VerifyEmail.ts
@@ -0,0 +1,27 @@
+import UserModel from '../../models/user.model';
+import { SendEmailFromTemplate } from '../Email';
+
+export const VerifyEmail = async (token: string) => {
+  const user = await UserModel.findOne({ verification_token: token });
+
+  if (!user) {
+    throw new Error('Invalid verification token');
+  }
+
+  await UserModel.updateOne(
+    { verification_token: token },
+    { is_verified: true, verification_token: null },
+  );
+
+  await SendEmailFromTemplate({
+    template: 'VerifyEmail',
+    from: '[email protected]',
+    to: user.email,
+    data: {
+      firstname: user.firstname,
+      email: user.email,
+    },
+  });
+
+  return true;
+};
