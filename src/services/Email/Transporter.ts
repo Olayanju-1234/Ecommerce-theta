f3a9c40961ff520e77e43abfde136d217caa0471
Joseph Olayanju <horlarhyanjuhjoseph@gmail.com>
2025-02-01 14:15:29 +0100

    removed logging

--- src/services/Email/Transporter.ts
+++ src/services/Email/Transporter.ts
@@ -3,12 +3,6 @@ import { config } from '../../config/env';
 
 const { EMAIL_PORT, EMAIL_HOST, EMAIL_SECURE, EMAIL, EMAIL_PASSWORD } = config;
 
-console.log('EMAIL_PORT', EMAIL_PORT);
-console.log('EMAIL_HOST', EMAIL_HOST);
-console.log('EMAIL_SECURE', EMAIL_SECURE);
-console.log('EMAIL', EMAIL);
-console.log('EMAIL_PASSWORD', EMAIL_PASSWORD);
-
 export default createTransport({
   host: EMAIL_HOST,
   port: EMAIL_PORT,
