30167f24d46dd2532b6f8139044b9ee4abdd13ce
Joseph Olayanju <horlarhyanjuhjoseph@gmail.com>
2025-02-03 08:53:43 +0100

    feat: added swagger config

---
+++ src/config/swagger.ts
@@ -0,0 +1,28 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { Express } from 'express';
+
+const swaggerOptions: swaggerJSDoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Your API Name',
+      version: '1.0.0',
+      description: 'API Documentation using Swagger',
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000',
+        description: 'Local server',
+      },
+    ],
+  },
+  apis: ['./src/docs/*.ts'],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+export const setupSwagger = (app: Express) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  console.log('📄 Swagger Docs available at: http://localhost:5000/api-docs');
+};
